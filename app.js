4329c812e41508e2fab15db8cb34efacd016b6f5
Winstler <84248453+Winstler@users.noreply.github.com>
2021-07-12 13:01:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+(async function(){
+                const URL_currencies = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+                let curencies = await fetch(URL_currencies);
+                curencies = await curencies.json();
+
+                console.log(curencies);
+
+                const URL_countries = 'https://restcountries.eu/rest/v2/all';
+                let countries = await fetch(URL_countries);
+                countries = await countries.json();
+
+                console.log(countries);
+
+                let countries_true = []
+
+                function countries_check (a, b){ //a == currencies, b == countries
+                    for (let i = 0;i<=a.length-1;i++){
+                        for (let j = 0; j <= b.length-1 ;j++){
+                            if(a[i].cc == b[j].currencies[0].code){
+                                countries_true.push(b[j]);
+                                countries_true[countries_true.length-1].valuta = b[j].currencies[0].code;
+                            }
+                            else{
+                                if(b[j].currencies[1] != undefined && a[i].cc == b[j].currencies[1].code){
+                                    countries_true.push(b[j]);
+                                    countries_true[countries_true.length-1].valuta = b[j].currencies[1].code;
+                                }
+                                else{
+                                    if(b[j].currencies[2] != undefined && a[i].cc == b[j].currencies[2].code){
+                                        countries_true.push(b[j]);
+                                        countries_true[countries_true.length-1].valuta = b[j].currencies[2].code;
+                                    }
+                                    else{
+                                        continue;
+                                    }  
+                                }
+                            }
+                        }
+                    }
+                }
+                countries_check(curencies, countries);
+                console.log(countries_true);
+
+                let searched_cur = '';
+
+                let cur = '';
+                let date = curencies[0].exchangedate;
+
+                for (let k = 0; k <= countries_true.length - 1; k++){
+                    searched_cur = countries_true[k].valuta;
+                    for (let m = 0; m <= curencies.length - 1; m++){
+                        if (searched_cur == curencies[m].cc){
+                            cur = curencies[m].rate;
+                        }
+                        else{
+                            continue;
+                        }
+                } 
+                    document.write(`<p class='content'>${countries_true[k].name}(${countries_true[k].valuta})        Курс: ${cur};    Дата: ${date}</p><br>`);
+                }
+            })();
+
